273401e0aa39740e776a44ae777a562ff630d7f0
nicholusbriz <atbriz256@gmail.com>
2026-08-16 22:30:45 +0300

    update my project with the static files

--- atbriz/settings.py
+++ atbriz/settings.py
@@ -159,7 +159,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'myapp' / 'static',
     BASE_DIR / 'web' / 'static',
